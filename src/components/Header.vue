bdf50aad1afa6a36f5eb888788ac08844486c405
DESKTOP-DKRP8M1\Brandon <30314729+Brandon0514@users.noreply.github.com>
2022-09-03 23:33:00 +0800

    feat: handle mobile view header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-app-bar style="padding: 0px 90px" app color="primary" light elevation-on-scroll elevation="0">
+
+  <v-app-bar :style="{ ...styleComputed }" app color="primary" light elevation-on-scroll elevation="0">
 
     <v-img src="@/assets/easystore-logo-horizontal-white.png" alt=""
       contain   
       height="70%"
+      width="200px"
       position="left center"
     >
     </v-img>
 
     <v-spacer />
 
-    <v-list class="d-flex align-center" color="primary">
+    <!-- Full header -->
+    <v-list v-if="isMini" class="d-flex align-center" color="primary">
       <v-list-item link v-for="(menu, index) in menus" :key="index" :to="menu.route">
         <v-list-item-title class="header_title">
           {{menu.title}}
@@ -20,6 +23,21 @@
         Get Started
       </v-btn>
     </v-list>
+
+    <!-- Icon dropdown -->
+    <v-menu bottom left v-else>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn dark icon v-bind="attrs" v-on="on">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="(menu, index) in menus" :key="index" :to="menu.route">
+          <v-list-item-title>{{ menu.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
 
   </v-app-bar>
 </template>
@@ -42,7 +60,22 @@ export default {
           {title:'Our Customers', route:'our_customers'},
           {title:'Log in', route:'login'}
         ],
-        toggleMenu: false,
+      }
+    },
+
+    computed: {
+      isMini() {
+        switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return false
+            case 'sm': return false
+            case 'md': return false
+            case 'lg': return false
+            case 'xl': return true
+            default: return false
+        }
+      },
+      styleComputed() {
+        return this.isMini ? { 'padding': '0px 90px' } : { 'padding': '0px 0px' } ;
       }
     },
 
