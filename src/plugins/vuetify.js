1dd3ae0da8f5a39ad607d262fd0b6e2ec16e0a95
DESKTOP-DKRP8M1\Brandon <30314729+Brandon0514@users.noreply.github.com>
2022-09-03 18:12:41 +0800

    style: add project theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,19 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+
+    theme: {
+      options: {
+        customProperties: true,
+      },
+      themes: {
+        light: {
+          primary: "#2f65ce", 
+          secondary: "#FF5588",
+          background: '#292930',
+          dark_blue: '#093272'
+        },
+        
+      },
+    },
 });
