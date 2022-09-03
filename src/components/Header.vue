db53a2c59b151c622ff7a11c6feb5e9e222b697f
DESKTOP-DKRP8M1\Brandon <30314729+Brandon0514@users.noreply.github.com>
2022-09-03 18:13:33 +0800

    feat: created app bar and view app section

---
+++ src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app-bar style="padding: 0px 90px" app color="primary" light elevation-on-scroll elevation="0">
+
+    <v-img src="@/assets/easystore-logo-horizontal-white.png" alt=""
+      contain   
+      height="70%"
+      position="left center"
+    >
+    </v-img>
+
+    <v-spacer />
+
+    <v-list class="d-flex align-center" color="primary">
+      <v-list-item link v-for="(menu, index) in menus" :key="index" :to="menu.route">
+        <v-list-item-title class="header_title">
+          {{menu.title}}
+        </v-list-item-title>
+      </v-list-item>
+      <v-btn outlined color="white">
+        Get Started
+      </v-btn>
+    </v-list>
+
+  </v-app-bar>
+</template>
+
+<script>
+
+export default {
+    name: 'Header', 
+
+    mounted () {
+      console.log(this.$vuetify.breakpoint.name)
+    },
+    data() {
+      return{
+        menus:[
+          {title:'Product', route:'home'},
+          {title:'Pricing', route:'pricing'},
+          {title:'Solutions', route:'solutions'},
+          {title:'Resource', route:'resource'},
+          {title:'Our Customers', route:'our_customers'},
+          {title:'Log in', route:'login'}
+        ],
+        toggleMenu: false,
+      }
+    },
+
+};
+</script>
+
+<style>
+  .header_title {
+    color: rgb(255, 255, 255);
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", 微软雅黑, Roboto, Arial, sans-serif;
+  }
+</style>
---
+++ src/components/LandingComponent.vue
@@ -0,0 +1,89 @@
+<template>
+<div class="mobile_download_section">
+  <v-container class="pt-15">
+    <v-row>
+
+      <!-- left side -->
+      <v-col lg="6">
+
+        <v-row>
+          <v-col lg="12" class="mt-1">
+              <v-btn color="black" class="py-8 mr-5 btn_view_app">
+                <v-img src="@/assets/cart-icon.png" alt=""
+                  contain
+                  width="50px"
+                >
+                </v-img>
+              </v-btn>
+
+              <v-btn color="white" class="dark_blue--text py mr--10 pa-8 text-transform-none btn_title_app btn_view_app">
+                {{shopping_title}}
+              </v-btn>
+            </v-col>
+        </v-row>
+
+        <h2 class="intro-text white--text mt-5">{{title}}</h2>
+        <p class="description-text white--text mt-5">{{description}}</p>
+        <v-btn color="secondary" class="white--text mr--10 mt-5 pa-8 btn_title_app btn_view_app text-transform-none">{{view_app_descrip}}</v-btn>
+      </v-col>
+
+      <!-- right side -->
+      <v-col lg="6">
+        <v-img src="@/assets/Artboard-8.png" alt=""
+          contain   
+          width="100%"
+        >
+        </v-img>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'LandingComponent', 
+
+    data() {
+      return{
+        title: "Convert your store into a mobile app",
+        description: "Shopping App - an app drives more conversions and repeat purchases for your business",
+        view_app_descrip: "Explore Shopping App",
+        shopping_title: "Shopping App"
+      }
+    },
+
+};
+</script>
+
+<style scoped>
+
+  h2.intro-text{
+    font-size: 60px;
+    font-weight: bold;
+  }
+
+  p.description-text{
+    font-size: 20px;
+  }
+
+  button.btn_title_app.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light {
+    font-size: 20px;
+    font-weight: bolder;
+  }
+
+  button.btn_view_app.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light{
+    border-radius: 10px;
+  }
+
+  .text-transform-none{
+    text-transform: unset !important;
+  }
+
+  div.mobile_download_section {
+    background-color:  var(--v-primary-base);
+  }
+
+</style>
